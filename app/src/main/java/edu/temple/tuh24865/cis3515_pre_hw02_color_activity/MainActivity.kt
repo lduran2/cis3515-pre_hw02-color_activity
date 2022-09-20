5a6b95da10df05dd9c2aedfac57f6583b7567150
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2022-09-20 11:17:45 -0400

    start project

---
+++ app/src/main/java/edu/temple/tuh24865/cis3515_pre_hw02_color_activity/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.temple.tuh24865.cis3515_pre_hw02_color_activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/edu/temple/tuh24865/cis3515_pre_hw02_color_activity/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package edu.temple.tuh24865.cis3515_pre_hw02_color_activity
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
